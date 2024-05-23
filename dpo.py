871b18e686abc6ecd08d5a6b1f856cb37d146b98
Matthew Hayes <mattjhayes3@gmail.com>
2024-05-22 18:02:28 -0700

    dpo

--- dpo.py
+++ dpo.py
@@ -113,9 +113,11 @@ if __name__ == "__main__":
         quantization_config=quantization_config,
     )
     model = AutoModelForCausalLM.from_pretrained(model_config.model_name_or_path, **model_kwargs)
+    print(f"Load model ", model_config.model_name_or_path)
     peft_config = get_peft_config(model_config)
     if peft_config is None:
         model_ref = AutoModelForCausalLM.from_pretrained(model_config.model_name_or_path, **model_kwargs)
+        print(f"Load ref model ", model_config.model_name_or_path)
     else:
         model_ref = None
     tokenizer = AutoTokenizer.from_pretrained(model_config.model_name_or_path)
