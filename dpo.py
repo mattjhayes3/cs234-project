d2ea54493cc2378d7014dc9614575b68315a49ca
Matthew Hayes <mattjhayes3@gmail.com>
2024-05-31 05:33:11 -0700

    fix eval

--- dpo.py
+++ dpo.py
@@ -207,4 +207,4 @@ if __name__ == "__main__":
             r, r_sem, kl, kl_sem = ppo.eval(checkpoint, f"epoch {epoch}")
             results.append([training_args.output_dir, training_args.beta, f"epoch {epoch}", r, r_sem, kl, kl_sem])
         for result in results:
-            print(",".join(result))
+            print(",".join([str(r) for r in result]))
